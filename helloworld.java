25a85504d5a4b5acc37464572be8d5e282f277ce
l120191 <licia.belli@gmail.com>
2015-03-20 17:13:35 +0100

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,11 @@
 class HelloWorld{
 	public static void main(String[] args) {
+	
 		Print p=new Print();
 		p.helloworld();
+		
+		Calculator c = new Calculator(2,4,'+');
+	 	System.out.println(c.result());	
+		
 	}
 }
